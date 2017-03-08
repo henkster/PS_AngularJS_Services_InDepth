f33a158dd093b601edbba2822aef4b502f9c65e1
Steve Henke <SHenke@paylocity.com>
2017-03-08 08:26:50 -0600

    Using provider module function instead, but with configuration.

--- public/app/app.js
+++ public/app/app.js
@@ -2,19 +2,33 @@
 
   var app = angular.module('app', []);
 
-  app.config(function($provide) {
-      
-    $provide.provider('books', function() {
-        this.$get = function() {
+  app.provider('books', function() {
+        this.$get = function() { // $get must be define.
 
           var appName = 'Book Logger';
           var appDesc = 'Track whick books you read.'
 
+          var version = '1.0';
+
+          if (includeVersionInTitle) {
+            appName += ' ' + version;
+          }
+
           return {
-            appName: appName,
+            appName: appName, // these are just random props, ie appName and appDesc aren't required.
             appDesc: appDesc
           };
         };
+
+        var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function(value) {
+          includeVersionInTitle = value;
+        };
     });
+
+  app.config(function(booksProvider) { // Angular automatically appends provider to service.
+      
+      booksProvider.setIncludeVersionInTitle(false);
+    
   });
 }());
