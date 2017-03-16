d6ad1742795d66866f2a69a2858529c65c702692
Steve Henke <SHenke@paylocity.com>
2017-03-16 09:17:50 -0500

    Using chained catch statement.

--- public/app/books/BooksController.js
+++ public/app/books/BooksController.js
@@ -1,34 +1,38 @@
 (function() {
 
-    angular.module('app')
-        .controller('BooksController', ['books', 'dataService', 'badgeService', BooksController]); // annotations - inline
+  angular.module('app')
+    .controller('BooksController', ['books', 'dataService', 'badgeService', BooksController]); // annotations - inline
 
 
-    function BooksController(books, dataService, badgeService) { // annotations - this is simplest form (just names), but minification would break.
+  function BooksController(books, dataService, badgeService) { // annotations - this is simplest form (just names), but minification would break.
 
-        var vm = this;
+    var vm = this;
 
-        vm.appName = books.appName;
+    vm.appName = books.appName;
 
-        dataService.getAllBooks()
-            .then(getBooksSuccess, getBooksError, getBooksNotification); // success, error, notification callback handling
-        
-        vm.allReaders = dataService.getAllReaders();
+    dataService.getAllBooks()
+      .then(getBooksSuccess, null, getBooksNotification) // success, error, notification callback handling
+      .catch(errorCallback);
+    
+    vm.allReaders = dataService.getAllReaders();
 
-        vm.getBadge = badgeService.retrieveBadge;
+    vm.getBadge = badgeService.retrieveBadge;
 
-        function getBooksSuccess(books) {
-            vm.allBooks = books;
-        }
-
-        function getBooksError(reason) {
-            console.log(reason);
-        }
-
-        function getBooksNotification(notification) {
-            console.log("Promise notification: " + notification);
-        }
+    function getBooksSuccess(books) {
+      throw 'Something happened in success handling!'
+      vm.allBooks = books;
     }
 
+    // function getBooksError(reason) {
+    //   console.log(reason);
+    // }
 
+    function errorCallback(error) {
+      console.log('Error message: ' + error);
+    }
+
+    function getBooksNotification(notification) {
+      console.log("Promise notification: " + notification);
+    }
+  }
 }());
